f16cfde862f7e4752479b290a79131db3c754ae7
yunweneric <yunweneric@gmail.com>
2020-11-03 10:19:47 +0100

    added routes semi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RestoListComponent } from './resto-list/resto-list.component';
+import { RestoUpdateComponent } from './resto-update/resto-update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"/", component:AppComponent},
+  {path:"register", component:RegisterComponent},
+  {path:"login", component:LoginComponent},
+  {path:"list", component:RestoListComponent},
+  {path:"update", component:RestoUpdateComponent},
+  {path:"home", component:AppComponent},
+  {path:"**", component:AppComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
