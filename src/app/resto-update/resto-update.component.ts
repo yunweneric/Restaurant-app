258eb593b53c51eee4d7fa4265ec9b1b9936a4c7
yunweneric <yunweneric@gmail.com>
2020-11-07 13:21:14 +0100

    updated add and update UI

--- src/app/resto-update/resto-update.component.ts
+++ src/app/resto-update/resto-update.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
+import { RestoService } from '../resto.service';
 @Component({
   selector: 'app-resto-update',
   templateUrl: './resto-update.component.html',
-  styleUrls: ['./resto-update.component.css']
+  styleUrls: ['./resto-update.component.css', '../shared/sharedcss.css']
 })
 export class RestoUpdateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private resto: RestoService) {}
+  ngOnInit(): void {}
+  alert: boolean = false;
 
-  ngOnInit(): void {
-  }
+  addresto = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    address: new FormControl(''),
+  });
+  collectResto() {
+    this.resto.saveResto(this.addresto.value).subscribe((data) => {
+      // console.log('data', data);
+    });
+    this.alert = true;
+    this.addresto.reset({})
 
+  };
 }
